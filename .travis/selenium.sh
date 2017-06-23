2a5c449be455379154dcc46099fdae9c5396c57a
Matthias Fischmann <mf@zerobuzz.net>
2017-06-23 17:24:52 +0200

    ...

--- .travis/selenium.sh
+++ .travis/selenium.sh
@@ -3,7 +3,7 @@
 set -e
 
 export PATH=$PATH:/usr/lib/chromium-browser
-export SEL=/selenium-server-standalone-2.53.1.jar
+export SEL=/selenium-server-standalone-3.4.0.jar
 # If you want to update selenium-grid, check out https://github.com/liqula/react-hs-docker/blob/master/Dockerfile.
 export SELENIUM_HOST=localhost
 export SELENIUM_HUB_PORT=4444
@@ -13,8 +13,14 @@ export DISPLAY=:1
 export VNC_PORT=5900
 export VNC_DIMS=800x600x8
 
-export SELENIUM_HUB_ARGS=-debug
-# (try `java -jar /selenium-server-standalone-2.53.1.jar [-role <node|hub>] -h` to learn more.)
+export SELENIUM_HUB_ARGS="-debug true"
+# (try `java -jar /selenium-server-standalone-<version>.jar [-role <node|hub>] -h` to learn more.)
+
+# chromedriver is started, but i the react-flux tests get a
+# timeout trying to open a page.  but who cannot connect to whom?
+# try standalone senelium grid with -log, -debug next!
+# also, is chrome driver connecting to the right display?  shouldn't we see it on vnc?
+
 
 case "$1" in
   start)
@@ -27,10 +33,10 @@ case "$1" in
     echo " ok"
 
 # if you want to run the react-hs-docker image locally and connect to selenium while it is running, uncomment this block.
-#    echo -n "starting vnc"
-#    nohup x11vnc -forever -usepw -shared -rfbport $VNC_PORT -display $DISPLAY > $LOG_PATH/selenium-x11vnc.log 2>&1 &
-#    until (nc -z $SELENIUM_HOST $VNC_PORT); do echo -n "."; sleep .$RANDOM; done
-#    echo " ok"
+    echo -n "starting vnc"
+    nohup x11vnc -forever -rfbport $VNC_PORT -display $DISPLAY > $LOG_PATH/selenium-x11vnc.log 2>&1 &
+    until (nc -z $SELENIUM_HOST $VNC_PORT); do echo -n "."; sleep .$RANDOM; done
+    echo " ok"
 
     echo -n "starting hub"
     nohup java -jar $SEL -role hub -host $SELENIUM_HOST -port $SELENIUM_HUB_PORT \
